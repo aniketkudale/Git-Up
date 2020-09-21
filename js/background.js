e0b87e15abada764390c741e4b36373da05c5554
Aniket Kudale <aniket.kudale@hotmail.com>
2020-09-21 11:42:58 +0530

    Delete background.js

--- js/background.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-	var readme = document.getElementById('readme');
-	readme.parentNode.prepend(readme);
-	readme.style.marginBottom = '20px';
-});
